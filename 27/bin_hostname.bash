eaa8a4d5978442209bd2b0cfde02de5c4d91b705
peluza <edisonisaza@gmail.com>
2020-04-16 20:57:28 -0500

    hostname

---
+++ 27/bin_hostname.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Checks if your shell handle the ; command separator
+
+shell="the date of the hostname"
+hsh="the date of the hostname"
+
+command="hostname"
+
+# clean up
+rm -f $shell
+rm -f $hsh
+
+# create a pseudo random file
+touch $shell
+touch $hsh
+
+# send commands
+echo "$command" | ./hsh > $shell 2> /dev/null &
+echo "$command" | /bin/sh > $hsh 2> /dev/null &
+
+# wait a little bit -> HOMEWORK
+$SLEEP $SLEEPSECONDS
+
+# check the result
+nmatch=`grep -c "$shell" "$hsh"`
+if [ $nmatch -eq 0 ]; then
+	   echo "OK"
+else
+	   echo "Failed"
+fi
+
+# clean up
+rm -f $shell
+rm -f $hsh
+pkill hsh
